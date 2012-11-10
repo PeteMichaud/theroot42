e7d7852791a3101caa05c25757b1e236b9b5e6d7
Pete Michaud <michaudp@gmail.com>
2012-11-10 01:39:32 -0600

    refactored some comment code into Tag.to_tag_array

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -21,6 +21,42 @@ class Tag < ActiveRecord::Base
     new_tags
   end
 
+  def self.to_tag_array tags, opts = {}
+
+    # Build Options
+    opts = {
+        as_param: false,
+        delimiter: '#'
+    }.merge(opts)
+
+    # Build array out of whatever was passed in
+    name_array = []
+    if tags.is_a? Array
+      name_array = tags
+    elsif tags.is_a? String
+      name_array = Tag.parse(tags, opts[:delimiter])
+    elsif tags.is_a? Tag
+      name_array << tags
+    else
+      raise "Expecting an array, string, or Tag, got: #{tags.class}"
+    end
+
+    # Build an array of Tags from the name array
+    if name_array.first.is_a? String
+      attr = opts[:as_param] ? 'param_name' : 'name'
+      name_array.map! { |n| n.parameterize } if opts[:as_param]
+      tag_array = name_array.map do |t|
+        Tag.send("find_by_#{attr}", t)
+      end.delete_if { |t| t.nil? }
+    elsif name_array.first.is_a? Tag
+      tag_array = name_array
+    else
+      raise "Tags must be either strings or Tag objects. Instead got type #{name_array.first.class}"
+    end
+
+    tag_array
+  end
+
   def count
     Tagging.where(tag_id: self.id).count
   end
