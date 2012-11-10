6b1eae526d72743638c2a406ae345f7604e83bba
Pete Michaud <michaudp@gmail.com>
2012-11-09 19:14:47 -0600

    main index checks for tag existence

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,12 @@ class MainController < ApplicationController
 
   def index
     if params.has_key? :tag
-      @comments = CommentDecorator.decorate(Comment.tagged_with(params[:tag]))
+      tag = Tag.find_by_param_name(params[:tag])
+      if tag.present?
+        @comments = CommentDecorator.decorate(Comment.tagged_with(tag))
+      else
+        redirect_to new_thread_path
+      end
     else
       @comments = CommentDecorator.all
     end
