c780339cc67297f2a0b4441a9fb4920c2efa73a4
Pete Michaud <michaudp@gmail.com>
2012-11-13 20:12:17 -0600

    added vote route, and moved tag_comment route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Theroot::Application.routes.draw do
   devise_for :users
 
   resources :users
-  resources :comments
+  resources :comments do
+    member do
+      post '/tag_comment' => 'comments#tag_comment', as: :tag_comment
+      post '/vote' => 'comments#vote', as: :vote
+    end
+  end
   resources :tags
   resources :votes
 
   get '/t/:tag' => 'main#index', as: :t
   get '/new' => 'main#new', as: :new_thread
-  post '/tag_comment' => 'comments#tag_comment', as: :tag_comment
 
   root to: 'main#home'
 end
