1eb271cce3a775993ee58af5b8f004e26bf619c8
Pete Michaud <michaudp@gmail.com>
2012-11-15 18:48:27 -0600

    added previous page link helper

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,8 +2,23 @@ module MainHelper
 
   def next_page_link
     if @page != :last && @comments_total > (@page + 1) * @page_size
-      content_tag :a, href: t_path(params[:tag], @page+2), class: 'next_page' do
+      content_tag :a, href: t_path(params[:tag], @page+2), class: 'next_page', data: {page: @page+2} do
         "Page #{@page+2}"
+      end
+    end
+  end
+
+  def prev_page_link
+    if @page == :last
+      if @comments_total > @page_size
+        @page == 0
+      else
+        @page = (@comments_total / @page_size).ceil
+      end
+    end
+    if @page > 0
+      content_tag :a, href: t_path(params[:tag], @page), class: 'prev_page', data: {page: @page} do
+        "Page #{@page}"
       end
     end
   end
