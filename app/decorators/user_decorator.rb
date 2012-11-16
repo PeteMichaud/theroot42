4667b2cca5bca7f2049a78777969e0997d9cbf89
Pete Michaud <michaudp@gmail.com>
2012-11-16 02:22:03 -0600

    correctly loads default avatar

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,7 +2,7 @@ class UserDecorator < Draper::Base
   decorates :user
 
   def render_avatar
-    h.image_tag model.avatar.url unless model.avatar.url.nil?
+    return h.image_tag model.avatar.url if model.avatar.file?
     h.image_tag "/assets/#{Theroot::Application.config.default_avatar}"
   end
 
