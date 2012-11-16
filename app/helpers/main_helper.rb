8880d1c99b7cd52fe036699a9b67b0edaa021e0e
Pete Michaud <michaudp@gmail.com>
2012-11-16 16:09:13 -0600

    refactored next page link and prev page link

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,26 +1,27 @@
 module MainHelper
 
-  def next_page_link
-    if @page != :last && @comments_total > (@page + 1) * @page_size
-      content_tag :a, href: t_path(params[:tag], @page+2), class: 'next_page', data: {page: @page+2} do
+  def next_page_link path, total
+    if @page != :last && total > (@page + 1) * @page_size
+      content_tag :a, href: "#{path}/#{@page + 2}", class: 'next_page', data: {page: @page+2} do
         "Next Page (#{@page+2})"
       end
     end
   end
 
-  def prev_page_link
+  def prev_page_link path, total
     if @page == :last
-      if @page_size > @comments_total
+      if @page_size > total
         @page = 0
       else
-        @page = (@comments_total.to_f / @page_size).ceil
+        @page = (total.to_f / @page_size).ceil
       end
     end
     if @page > 0
-      content_tag :a, href: t_path(params[:tag], @page), class: 'prev_page', data: {page: @page} do
+      content_tag :a, href: "#{path}/#{@page}", class: 'prev_page', data: {page: @page} do
         "Previous Page (#{@page})"
       end
     end
   end
+
 
 end
