036e28c532d8875ab441a9c8d31ff6f791b0732c
Pete Michaud <michaudp@gmail.com>
2012-11-15 18:55:57 -0600

    improved the look of next and previous buttons

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,7 +3,7 @@ module MainHelper
   def next_page_link
     if @page != :last && @comments_total > (@page + 1) * @page_size
       content_tag :a, href: t_path(params[:tag], @page+2), class: 'next_page', data: {page: @page+2} do
-        "Page #{@page+2}"
+        "Next Page (#{@page+2})"
       end
     end
   end
@@ -18,7 +18,7 @@ module MainHelper
     end
     if @page > 0
       content_tag :a, href: t_path(params[:tag], @page), class: 'prev_page', data: {page: @page} do
-        "Page #{@page}"
+        "Previous Page (#{@page})"
       end
     end
   end
